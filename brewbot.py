72c970a27dcc60e74f427285e84581a6f82ba2c1
rathmourn <56705032+rathmourn@users.noreply.github.com>
2019-10-18 13:01:08 -0400

    Restructuring.

--- brewbot.py
+++ brewbot.py
@@ -1,31 +1,6 @@
-import yaml
-import discord
 from discord.ext import commands
+import discord
 
-bot = commands.Bot(command_prefix='$')
+import config
 
-# Notification event for when the bot is up and running
-@bot.event
-async def on_ready():
-    print("Bot is ready.")
-
-
-# Main message handler
-@bot.event
-async def on_message(ctx):
-    # Process the other commands first
-    await bot.process_commands(ctx)
-
-
-def main():
-    # Load configuration file
-    with open("config.yml", 'r') as ymlfile:
-        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
-
-    # Launch the bot
-    bot.run(cfg['bot']['discord_api_key'])
-
-
-if __name__ == '__main__':
-    main()
 
