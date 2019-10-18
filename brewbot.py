632e3c3745575005855338ea85bdf620405d24f7
rathmourn <56705032+rathmourn@users.noreply.github.com>
2019-10-18 14:22:52 -0400

    Finally sorted out cog and extension architecture with discord.py

--- brewbot.py
+++ brewbot.py
@@ -1,6 +1,23 @@
-from discord.ext import commands
 import discord
+from discord.ext import commands
 
 import config
 
+
+class BrewBot(discord.ext.commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix=config.COMMAND_PREFIX)
+
+        # Load the bot's modules
+        for module in config.BOT_MODULES:
+            self.load_extension(module)
+
+    async def on_message(self, message):
+        await super().process_commands(message)
+
+    async def on_ready(self):
+        print("Bot is Ready")
+
+    def run(self):
+        super().run(config.DISCORD_BOT_TOKEN)
 
---
+++ launcher.py
@@ -0,0 +1,10 @@
+import brewbot
+
+
+def main():
+    bot = brewbot.BrewBot()
+    bot.run()
+
+
+if __name__ == '__main__':
+    main()
