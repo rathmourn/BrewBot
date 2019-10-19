b108b8fd3e3ded8ee7d01e005808c2f1f274c269
rathmourn <56705032+rathmourn@users.noreply.github.com>
2019-10-19 09:02:04 -0400

    Database tool and addition of db_session to BrewBot class.

--- launcher.py
+++ launcher.py
@@ -1,9 +1,28 @@
 import brewbot
+import config
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 
 def main():
-    bot = brewbot.BrewBot()
+    dirpath = os.getcwd()
+    print("current directory is : " + dirpath)
+    foldername = os.path.basename(dirpath)
+    print("Directory name is : " + foldername)
+
+    Base = declarative_base(())
+    db_engine = create_engine('sqlite:///' + config.BOT_DB_NAME)
+    Base.metadata.bind = db_engine
+    db_session_maker = sessionmaker(bind=db_engine)
+    db_session = db_session_maker()
+
+    bot = brewbot.BrewBot(db_session)
     bot.run()
+
+    db_session.close()
 
 
 if __name__ == '__main__':
